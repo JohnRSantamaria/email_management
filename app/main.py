48ed8b72ad8cfe795d8e2c52ffc1f4989e6dfa43
JohnRSantamaria <santamariajohnd@gmail.com>
2025-05-04 12:29:55 -0500

    update en la ruta para gninx

--- app/main.py
+++ app/main.py
@@ -5,14 +5,16 @@ from datetime import datetime, timezone
 
 from app.routes import document
 
+app = FastAPI(root_path="/invoice-scanner")
+
+
 def format_error_response(error_message: str, error_type: str = "InternalError"):
     return {
         "detail": error_message,
         "error_type": error_type,
-        "timestamp": datetime.now(timezone.utc).isoformat()
+        "timestamp": datetime.now(timezone.utc).isoformat(),
     }
 
-app = FastAPI()
 
 # Handler para errores HTTPException (por ejemplo 404, 403, etc.)
 @app.exception_handler(HTTPException)
@@ -20,9 +22,8 @@ async def http_exception_handler(request: Request, exc: HTTPException):
     return JSONResponse(
         status_code=exc.status_code,
         content=format_error_response(
-            error_message=str(exc.detail),
-            error_type="HTTPException"
-        )
+            error_message=str(exc.detail), error_type="HTTPException"
+        ),
     )
 
 
@@ -31,10 +32,7 @@ async def http_exception_handler(request: Request, exc: HTTPException):
 async def general_exception_handler(request: Request, exc: Exception):
     return JSONResponse(
         status_code=500,
-        content=format_error_response(
-            error_message=str(exc),
-            error_type="Exception"
-        )
+        content=format_error_response(error_message=str(exc), error_type="Exception"),
     )
 
 
